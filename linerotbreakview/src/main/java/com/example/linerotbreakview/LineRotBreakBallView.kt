3c05232470d060cf99e2562b5b307ca9b860a6af
Anwesh43 <anweshthecool0@gmail.com>
2021-05-06 01:47:58 +0530

    adding constants and libraries

--- linerotbreakview/src/main/java/com/example/linerotbreakview/LineRotBreakBallView.kt
+++ linerotbreakview/src/main/java/com/example/linerotbreakview/LineRotBreakBallView.kt
@@ -1,1 +1,26 @@
 package com.example.linerotbreakview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#1A237E",
+    "#004D40",
+    "#0091EA",
+    "#DD2C00"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.6f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
