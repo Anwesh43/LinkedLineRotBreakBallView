02b4c985e28da25f67bf128bbc1da8cefce5cb56
Anwesh43 <anweshthecool0@gmail.com>
2021-05-06 02:41:21 +0530

    adding LineRotBreakBall class

--- linerotbreakview/src/main/java/com/example/linerotbreakview/LineRotBreakBallView.kt
+++ linerotbreakview/src/main/java/com/example/linerotbreakview/LineRotBreakBallView.kt
@@ -160,4 +160,27 @@ class LineRotBreakBallView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotBreakBall(var i : Int) {
+
+        private var curr : LRBBNode = LRBBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
